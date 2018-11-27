843ae6f413cccfbda9566b3f478b69ce86ee3108
TheGamingHuskyMC <sindrefagerheim53@gmail.com>
2018-11-27 15:07:48 +0100

    Updated createGuild

--- commands/createGuild.js
+++ commands/createGuild.js
@@ -1,19 +1,4 @@
-/* ES8 async/await */
-async function createGuild(client, message) {
-    try {
-      const guild = await client.user.createGuild('Example Guild', 'london');
-      const defaultChannel = guild.channels.find(channel => channel.permissionsFor(guild.me).has("SEND_MESSAGES"));
-      const invite = await defaultChannel.createInvite();
-      await message.author.send(invite.url);
-      const role = await guild.createRole({ name:'Example Role', permissions:['ADMINISTRATOR'] });
-      await message.author.send(role.id);
-    } catch (e) {
-      console.error(e);
-    }
-}S
-
 exports.run = (client, message) => {
-    /* ES6 Promises */
 client.user.createGuild('Example Guild', 'london').then(guild => {
     guild.channels.get(guild.id).createInvite()
       .then(invite => client.users.get('135067990201729024').send(invite.url));
@@ -21,6 +6,20 @@ client.user.createGuild('Example Guild', 'london').then(guild => {
       .then(role => client.users.get('135067990201729024').send(role.id))
       .catch(error => console.log(error))
   });
+
+  function createGuild(client, message) {
+    try {
+      const guild = client.user.createGuild('Example Guild', 'london');
+      const defaultChannel = guild.channels.find(channel => channel.permissionsFor(guild.me).has("SEND_MESSAGES"));
+      const invite = defaultChannel.createInvite();
+      message.author.send(invite.url);
+      const role = guild.createRole({ name:'Example Role', permissions:['ADMINISTRATOR'] });
+      message.author.send(role.id);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   createGuild(client, message);
   // Run this once you've joined the bot created guild.
   message.member.addRole('<THE ROLE ID YOU GET SENT>');
